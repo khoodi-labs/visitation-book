5e17c177eb4f0ae4e639652f60788a4f7d68d42f
Muyinda Rogers <moverr@gmail.com>
2023-09-09 05:11:25 +0300

    :DEbuggging :

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,10 +1,30 @@
 
 
 export default function AuthService(){
+    
+    const user1 = {
+        username: "moverr@gmail.com",
+        password:"password"
+    }
 
+    const user2 = {
+        username: "rita@gmail.com",
+        password:"password"
+    }
+
+    const users = [user1,user2];
+    
+    //todo: simulating the service structure 
     function validateUsernameAndPassword(username,password){
 
-        return true;
+        if(users != null){
+            users.forEach( (user) =>{
+                if(user.password == password && user.username === username)
+                    return true;
+
+            });
+        }
+        return false;
     }
 
     return {
