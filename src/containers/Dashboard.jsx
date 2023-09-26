356b9bf0d0d132c52722e47a61e477525f85a1ea
Muyinda Rogers <moverr@gmail.com>
2023-09-27 02:01:47 +0300

    :Debuggging :

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -2,6 +2,125 @@ import React from 'react'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+
+<div className="container-fluid">
+	<div className="row">
+		<div className="col-md-12">
+
+			<div className="row">
+				<div className="col-md-2">
+        <h1>Side Bar</h1>
+          <ul>
+          
+            <li>  Visitation Requests</li>
+            <li>  Visitations </li>
+            </ul> 
+				</div>
+				<div className="col-md-10">
+       
+			<table className="table">
+				<thead>
+					<tr>
+						<th>
+							#
+						</th>
+						<th>
+							Product
+						</th>
+						<th>
+							Payment Taken
+						</th>
+						<th>
+							Status
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							1
+						</td>
+						<td>
+							TB - Monthly
+						</td>
+						<td>
+							01/04/2012
+						</td>
+						<td>
+							Default
+						</td>
+					</tr>
+					<tr className="table-active">
+						<td>
+							1
+						</td>
+						<td>
+							TB - Monthly
+						</td>
+						<td>
+							01/04/2012
+						</td>
+						<td>
+							Approved
+						</td>
+					</tr>
+					<tr class="table-success">
+						<td>
+							2
+						</td>
+						<td>
+							TB - Monthly
+						</td>
+						<td>
+							02/04/2012
+						</td>
+						<td>
+							Declined
+						</td>
+					</tr>
+					<tr className="table-warning">
+						<td>
+							3
+						</td>
+						<td>
+							TB - Monthly
+						</td>
+						<td>
+							03/04/2012
+						</td>
+						<td>
+							Pending
+						</td>
+					</tr>
+					<tr className="table-danger">
+						<td>
+							4
+						</td>
+						<td>
+							TB - Monthly
+						</td>
+						<td>
+							04/04/2012
+						</td>
+						<td>
+							Call in to confirm
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		 
+
+
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+    </div>
   )
 }
