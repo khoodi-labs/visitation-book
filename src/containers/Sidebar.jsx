061baa973a9b52a482d7200a9853efe8d423b092
Muyinda Rogers <moverr@gmail.com>
2023-10-04 23:59:49 +0300

    linking in dashboard :

--- src/containers/Sidebar.jsx
+++ src/containers/Sidebar.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Sidebar(props) {
   return (
-   
-        	<div className="col-md-2">
-								<h1>Dashboard</h1>
-								<ul>
-
-									<li>  Visitation Requests</li>
-									<li>  Visitations </li>
-								</ul>
-							</div>
- 
-  )
+    <div className="col-md-2">
+      <h1>Dashboard</h1>
+      <ul>
+        <li>
+          <Link to="/dashboard">Requests</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Visitations </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-Sidebar.propTypes = {}
+Sidebar.propTypes = {};
 
-export default Sidebar
+export default Sidebar;
