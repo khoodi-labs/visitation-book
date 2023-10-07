875b94a705033b1a36165e093b9ec086fb3706eb
Muyinda Rogers <moverr@gmail.com>
2023-10-08 00:11:24 +0300

    top sleave :

--- src/components/Common/TopSleave.jsx
+++ src/components/Common/TopSleave.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import '../../assets/css/Topsleave.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload, faUpload, faPrint, faSearch } from '@fortawesome/free-solid-svg-icons'; // Replace with the desired search icon
+
 
 function TopSleave(props) {
   return (
     <div className="top-sleave">
-      <form className="form-inline">
+      <form className="form-inline"> 
         <label>Search: </label>
+        
         <input className="form-control mr-sm-2" type="text" />
         <button className="btn btn-primary my-2 my-sm-0" type="submit">
-          Search
+          <FontAwesomeIcon icon={faSearch}  />
+        </button>
+        <button className="btn btn-primary my-2 my-sm-0" type="submit">
+          <FontAwesomeIcon icon={faDownload}  />
         </button>
         <button className="btn btn-primary my-2 my-sm-0" type="button">
-          Export
+        <FontAwesomeIcon icon={faUpload}  />
         </button>
         <button className="btn btn-primary my-2 my-sm-0" type="button">
-          Import
+        <FontAwesomeIcon icon={faPrint}  />
         </button>
-        <button className="btn btn-primary my-2 my-sm-0" type="button">
-          New
-        </button>
-        <button className="btn btn-primary my-2 my-sm-0" type="button">
-          Print
-        </button>
+        
       </form>
     </div>
   );
