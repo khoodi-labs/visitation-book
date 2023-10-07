c5bb5b0c393288898e1df01107db15c1c5f80eaa
Muyinda Rogers <moverr@gmail.com>
2023-10-07 22:18:14 +0300

    :Debuggging : khoodi

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -3,40 +3,28 @@ moverr@gmail.com 09/09/23
 */
 
 const AuthService = () => {
+  const user1 = {
+    username: "moverr@gmail.com",
+    password: "password",
+  };
 
-    const user1 = {
-        username: "moverr@gmail.com",
-        password: "password"
-    }
+  const user2 = {
+    username: "abc",
+    password: "abc",
+  };
 
-    const user2 = {
-        username: "abc",
-        password: "abc"
-    }
+  const users = [user1, user2];
 
-    const users = [user1, user2];
+  //todo: simulating the service structure
+  const validateUsernameAndPassword = (username, password) => {
+    return users.find((item) => {
+      return item.username === username && item.password === password;
+    });
+  };
 
-    //todo: simulating the service structure 
-    const validateUsernameAndPassword = (username, password) => {
-
-        let status = false;
-
-        users.forEach((item) => {
-            let userDetail = item;
-            if (username === userDetail.username && password === userDetail.password) {
-                status = true;
-                return status;
-            }
-
-        });
-
-        return status;
-    }
-
-    return {
-        validate: validateUsernameAndPassword
-    }
-}
-
+  return {
+    validate: validateUsernameAndPassword,
+  };
+};
 
 export default AuthService;
