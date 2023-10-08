ee3268b8335c026688bc2dbd702874b452b91cbc
Muyinda Rogers <moverr@gmail.com>
2023-10-08 21:31:02 +0300

    :Debugging : sidebar  :

--- src/containers/Sidebar.jsx
+++ src/containers/Sidebar.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import pingu1 from "../assets/images/pingu1.png";
 import visitorRequestsImage from "../assets/images/visitor-requests.png";
 import visitorVisitations from "../assets/images/visitor-visitations.png";
+import ListITmes from "../assets/images/listiems.png";
 
 import "../assets/css/Sidebar.css";
 
 function Sidebar(props) {
   return (
     <div className=" sidebar-container  sidebar ">
-      <img src={pingu1} />
+      <img src={ListITmes}  className="list-items-image"/>
       <ul>
         <li>
           <Link to="/dashboard/requests">
