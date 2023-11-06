2a416c25eb2d11803f863328d14431619b261074
Muyinda Rogers <moverr@gmail.com>
2023-11-06 05:53:16 +0300

    :Debuygging :

--- src/containers/Sidebar.jsx
+++ src/containers/Sidebar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import "../assets/css/Sidebar.css";
-import { faExpand, faMinimize, faDashboard, faCircleUser, faMaximize } from '@fortawesome/free-solid-svg-icons'; // Replace with the desired search icon
+import { faExpand, faMinimize, faDashboard, faCircleUser, faMaximize, faUsers, faBookOpen, faCogs } from '@fortawesome/free-solid-svg-icons'; // Replace with the desired search icon
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
@@ -23,20 +23,20 @@ function Sidebar(props) {
             <ul>
                 <li>
                     <Link to="/dashboard/requests/list">
-                        {expanded ? <span>  <FontAwesomeIcon icon={faDashboard} /> Dashboard</span> : <span>  <FontAwesomeIcon icon={faDashboard} />   </span>}
+                        {expanded ? <span>  <FontAwesomeIcon icon={faCogs} /> Dashboard</span> : <span>  <FontAwesomeIcon icon={faDashboard} />   </span>}
 
                     </Link>
                 </li>
 
                 <li>
                     <Link to="/dashboard/requests/list">
-                        {expanded ? <span>  <FontAwesomeIcon icon={faDashboard} /> Requests</span> : <span>  <FontAwesomeIcon icon={faDashboard} />   </span>}
+                        {expanded ? <span>  <FontAwesomeIcon icon={faUsers} /> Requests</span> : <span>  <FontAwesomeIcon icon={faDashboard} />   </span>}
 
                     </Link>
                 </li>
                 <li>
                     <Link to="/dashboard/visitations/list">
-                        {expanded ? <span>  <FontAwesomeIcon icon={faDashboard} /> Invitations</span> : <span>  <FontAwesomeIcon icon={faDashboard} />   </span>}
+                        {expanded ? <span>  <FontAwesomeIcon icon={faBookOpen} /> Invitations</span> : <span>  <FontAwesomeIcon icon={faDashboard} />   </span>}
 
                     </Link>
                 </li>
