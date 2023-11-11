5ab1c2a31d18a093ae1dae00c0311002afe1090d
Muyinda Rogers <moverr@gmail.com>
2023-11-11 23:47:39 +0300

    :Debugging :

--- src/containers/RequestsContainer/AddRequest.jsx
+++ src/containers/RequestsContainer/AddRequest.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import AlertElement from '../../components/Common/AlertElement'
 import TopSleave from '../../components/Common/TopSearchSleave'
@@ -14,113 +14,147 @@ import 'react-clock/dist/Clock.css';
 
 function AddRequest(props) {
 
-  
+
   const [alertSet, showAlert] = useState(false);
   const [timeInDate, changeTimeIn] = useState(new Date());
 
   const [timeOutDate, changeTimeOut] = useState(timeInDate);
 
+ 
+
+
+
+  const departmentData = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+
+  const fillDepartmentData = (data) => {
+    return  data != undefined &&  data != null && data.length > 0 ?
+
+      <div className="form-group col-md-4">
+
+        <label for="department_data">
+          Department
+        </label>
+        <SelectElement data={departmentData} required />
+      </div>
+      : ""
+  }
+
+  const fillOfficeData = (data) => {
+    return  data != undefined &&  data != null && data.length > 0  ?
+
+      <div className="form-group col-md-4">
+
+        <label for="office_data">
+          Office
+        </label>
+        <SelectElement data={departmentData} required />
+      </div>
+      : ""
+  }
+
+
+  const fillHostData = (data) => {
+    return   data != undefined &&  data != null && data.length > 0  ?
+
+      <div className="form-group col-md-4">
+
+        <label for="host_data">
+          Host
+        </label>
+        <SelectElement data={departmentData} required />
+      </div>
+      : ""
+  }
+
+
+
   return (
     <div>
-    <AlertElement cssClass={alertSet === true ? "alert alert-info alert-dismissable" : "hide"} msgtype="info" msgDetail="Processing..." />
-    <TabsElement active_tab="add" list_url="/dashboard/visitations/list" add_url="/dashboard/visitations/add" />
+      <AlertElement cssClass={alertSet === true ? "alert alert-info alert-dismissable" : "hide"} msgtype="info" msgDetail="Processing..." />
+      <TabsElement active_tab="add" list_url="/dashboard/visitations/list" add_url="/dashboard/visitations/add" />
 
 
-    <div className="container-fluid main-wrapper">
-      <div className="row">
-        <div className="col-md-12">
-          <form role="form" method="POST">
+      <div className="container-fluid main-wrapper">
+        <div className="row">
+          <div className="col-md-12">
+            <form role="form" method="POST">
 
-            <fieldset className="row">
-              <legend>Personal Information:</legend>
+              <fieldset className="row">
+                <legend>Personal Information:</legend>
 
-              <div className="form-group col-md-6">
+                <div className="form-group col-md-6">
 
-                <label for="first_name">
-                  First Name
-                </label>
-                <input type="text" className="form-control" id="first_name" />
-              </div>
+                  <label for="first_name">
+                    First Name
+                  </label>
+                  <input type="text" className="form-control" id="first_name" required />
+                </div>
 
-              <div className="form-group col-md-6">
+                <div className="form-group col-md-6">
 
-                <label for="first_name">
-                  Other Names
-                </label>
-                <input type="text" className="form-control" id="other_names" />
-              </div>
+                  <label for="first_name">
+                    Other Names
+                  </label>
+                  <input type="text" className="form-control" id="other_names" required />
+                </div>
 
-              <div className="form-group col-md-12">
+                <div className="form-group col-md-12">
 
-                <label for="first_name">
-                  Address :
-                </label>
-                <input autocomplete="home street-address" type="text" className="form-control" id="other_names" />
-              </div>
-            </fieldset> <br />
-            <fieldset className="row">
-              <legend>Host Information:</legend>
-
-              <div className="form-group col-md-4">
-
-                <label for="exampleInputEmail1">
-                  Department
-                </label>
-                <SelectElement />
-              </div>
-
-              <div className="form-group col-md-4">
-
-                <label for="exampleInputEmail1">
-                  Office
-                </label>
-                <SelectElement />
-              </div>
-
-              <div className="form-group col-md-4">
-
-                <label for="exampleInputEmail1">
-                  Host
-                </label>
-                <SelectElement />
-              </div>
-
-            </fieldset>
-            <br />
-            <fieldset className="row">
-              <legend>Time Information:</legend>
-              <div className="form-group col-md-6">
-
-                <label for="exampleInputPassword1">
-                  Time In : &nbsp;
-                </label>
-                <DateTimePicker minDate={new Date()} onChange={changeTimeIn} value={timeInDate} />
-              </div>
-
-              <div className="form-group col-md-6">
-
-                <label for="exampleInputPassword1">
-                TimeOut : &nbsp;
-                </label>
-                <DateTimePicker minDate={timeInDate} onChange={changeTimeOut} value={timeOutDate} />
-                
-                
-              </div>
-
-            </fieldset>
+                  <label for="address">
+                    Address :
+                  </label>
+                  <input autocomplete="home street-address" type="text" className="form-control" id="address" required />
+                </div>
+              </fieldset> <br />
+              <fieldset className="row">
+                <legend>Host Information:</legend>
 
 
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
+                {fillDepartmentData()}
+                {fillOfficeData()}
+                {fillHostData()}
+ 
+
+              </fieldset>
+              <br />
+              <fieldset className="row">
+                <legend>Time Information:</legend>
+                <div className="form-group col-md-6">
+
+                  <label for="exampleInputPassword1">
+                    Time In : &nbsp;
+                  </label>
+                  <DateTimePicker minDate={new Date()} onChange={changeTimeIn} value={timeInDate} />
+                </div>
+
+                <div className="form-group col-md-6">
+
+                  <label for="exampleInputPassword1">
+                    TimeOut : &nbsp;
+                  </label>
+                  <DateTimePicker minDate={timeInDate} onChange={changeTimeOut} value={timeOutDate} />
+
+
+                </div>
+
+              </fieldset>
+
+
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
+
+
+
     </div>
-
-
-
-  </div>
   )
 }
 
